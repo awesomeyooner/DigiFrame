b6d57eb5514bfd83b22779611a1e5d451448705e
Susan Yoon <awesomeyooner@gmail.com>
2025-07-05 19:22:23 -0700

    removed build

--- project/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dev/Documents/GitHub/DigiFrame/project
-# Build directory: /home/dev/Documents/GitHub/DigiFrame/project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/dev/Documents/GitHub/DigiFrame/project/build/Test[1]_include.cmake")
--- project/build/Test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/dev/Documents/GitHub/DigiFrame/project/build/Test[1]_tests.cmake")
-  include("/home/dev/Documents/GitHub/DigiFrame/project/build/Test[1]_tests.cmake")
-else()
-  add_test(Test_NOT_BUILT Test_NOT_BUILT)
-endif()
