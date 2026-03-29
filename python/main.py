cd883dff9e069ed26f0a2275fec0adba1e279b8d
Aaron Yoon <awesomeyooner@gmail.com>
2026-03-29 11:12:56 -0700

    cleaned up main

--- python/main.py
+++ python/main.py
@@ -7,17 +7,32 @@ from util import util
 import server
 import gui
 
+# The host IP Address
+HOST = '0.0.0.0'
+
+# The port to run the app on
+PORT = 8000
+
 def run_app():
-    server.app.run(host='0.0.0.0', port=8000, use_reloader=False)
+    server.app.run(host=HOST, port=PORT, use_reloader=False)
 
 def run_gui():
     gui.show()
     gui.exec()
 
-if __name__ == '__main__':
+def main():
+
+    print("==============================")
+    print("Starting up App...")
+    print("Click on the image and press ESC to quit")
+    print("==============================")
 
     flask_thread = multiprocessing.Process(target=run_app)
     flask_thread.daemon = True
     flask_thread.start()
 
     run_gui()
+
+if __name__ == '__main__':
+    main()
+    
