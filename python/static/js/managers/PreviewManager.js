355b0331a1b58568334017f3aa6038b2986e6a10
Aaron Yoon <awesomeyooner@gmail.com>
2026-04-08 21:02:54 -0700

    Fixed image scaling

--- python/static/js/managers/PreviewManager.js
+++ python/static/js/managers/PreviewManager.js
@@ -5,8 +5,8 @@ class PreviewManager
 
     static #position = new Point(0, 0);
 
-    static #maxWidth = getWidth();
-    static #maxHeight = getHeight();
+    static #maxWidth = 0;
+    static #maxHeight = 0;
 
     static #imageWidth = 0;
     static #imageHeight = 0;
@@ -21,18 +21,41 @@ class PreviewManager
             const srcWidth = this.#image.naturalWidth;
             const srcHeight = this.#image.naturalHeight;
             
-            var widthDiff = Math.abs(srcWidth - this.#maxWidth);
-            var heightDiff = Math.abs(srcHeight - this.#maxHeight);
+            var widthDiff = this.#maxWidth - srcWidth;
+            var heightDiff = this.#maxHeight - srcHeight;
 
-            // If you're closer to matching width
-            // Then make target width the max width
-            var scale = widthDiff < heightDiff ? this.#maxWidth / srcWidth : this.#maxHeight / srcHeight;
-            
+            var scale = 1;
+
+            // If the image is smaller in both dimensions that max
+            if(widthDiff > 0 && heightDiff > 0)
+            {
+                if(widthDiff < heightDiff)
+                    scale = Math.abs(this.#maxWidth / srcWidth);
+                else if(widthDiff > heightDiff)
+                    scale = Math.abs(this.#maxHeight / srcHeight);
+            }
+            // If the width is larger than the max but not height
+            else if(widthDiff < 0 && heightDiff > 0)
+                scale = Math.abs(this.#maxWidth / srcWidth);
+            // If the height is larger than max but not width
+            else if(widthDiff > 0 && heightDiff < 0)
+                scale = Math.abs(this.#maxHeight / srcHeight);
+            // If BOTH dimensions are larger than max
+            // Then the scale is the one that's farthest away from max
+            else if(widthDiff < 0 && heightDiff < 0)
+            {
+                if(Math.abs(widthDiff) > Math.abs(heightDiff))
+                    scale = Math.abs(this.#maxWidth / srcWidth);
+                else if(Math.abs(widthDiff) < Math.abs(heightDiff))
+                    scale = Math.abs(this.#maxHeight / srcHeight);
+            }
+
             var targetWidth = scale * srcWidth;
             var targetHeight = scale * srcHeight;
 
             this.setSize(targetWidth, targetHeight);
 
+            CanvasManager.drawCanvas();
             this.drawPreview();
         }
         
@@ -59,7 +82,11 @@ class PreviewManager
         return this.#position.getCenterOffset(this.#imageWidth, this.#imageHeight);
     }
 
-    static updateMax
+    static updateMaxDimensions()
+    {
+        this.#maxWidth = getWidth() * 0.5;
+        this.#maxHeight = getHeight() * 0.5;
+    }
 
     static drawPreview()
     {
@@ -67,8 +94,30 @@ class PreviewManager
             new Point(0, getHeight() / 5)
         );
 
-        this.#maxWidth = getWidth() * 0.5;
-        this.#maxHeight = getHeight() * 0.5;
+        this.updateMaxDimensions();
+
+        var edgePadding = Math.max(this.#maxWidth, this.#maxHeight) * 0.05;
+
+        context.fillStyle = "brown";
+        Shapes.rectangleCenterFromPoint(
+            this.#position,
+            this.#maxWidth + edgePadding,
+            this.#maxHeight + edgePadding
+        );
+
+        context.fillStyle = "gray";
+        Shapes.rectangleCenterFromPoint(
+            this.#position,
+            this.#maxWidth,
+            this.#maxHeight
+        );
+
+        context.fillStyle = "black";
+        Shapes.rectangleCenterFromPoint(
+            this.#position,
+            this.#imageWidth,
+            this.#imageHeight
+        );
 
         var centerPoint = this.getCenter();
 
