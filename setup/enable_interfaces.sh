3ee341f1331136d59c0b5bf4ab1a900f092a2e54
Aaron Yoon <awesomeyooner@gmail.com>
2026-06-11 00:45:43 -0700

    Added line to disable SPI CS in enable_interfaces.sh

--- setup/enable_interfaces.sh
+++ setup/enable_interfaces.sh
@@ -7,3 +7,23 @@ echo "Done!"
 echo "Enabling SPI..."
 sudo raspi-config nonint do_spi 0
 echo "Done!"
+
+FIRMWARE_FILE="/boot/firmware/config.txt"
+DISABLE_SPI_CS="dtoverlay=spi0-0cs"
+
+echo "Checking to see if SPI CS is disabled..."
+
+# If the SPI CS is already disabled
+# Then don't do anything
+if grep -q ${DISABLE_SPI_CS} ${FIRMWARE_FILE}; then
+	echo "SPI CS already disabled."
+# If not disabled, then disable it
+else
+	echo "SPI CS not disabled. Disabling..."
+	echo "" | sudo tee -a ${FIRMWARE_FILE} > /dev/null
+	echo "# Disables SPI CS, used for Inky Impression" | sudo tee -a ${FIRMWARE_FILE} > /dev/null
+	echo ${DISABLE_SPI_CS} | sudo tee -a ${FIRMWARE_FILE} > /dev/null
+	echo "Done!"
+fi
+
+echo "Please reboot to apply changes!"
